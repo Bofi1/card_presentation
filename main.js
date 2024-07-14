33d3efa045d0935ea1a6ff8865aaf73e05bf9812
bofi1 <angelborque03github@gmail.com>
2024-07-14 07:08:47 -0700

    jS update

---
+++ main.js
@@ -0,0 +1,11 @@
+(( )=>{ // funcion que se invoque automaticamente
+    
+    let card_container  = document.getElementById('card_container')
+    let card_number_text = document.getElementById('card_number_text')
+    let card_front  = document.getElementById('card_front')
+    let card_back  = document.getElementById('card_back')
+
+
+
+
+})()
