9b709eb7bb94afa2721421afdb28a693e4daaabf
bofi1 <angelborque03github@gmail.com>
2024-07-16 20:49:41 -0700

    card number synchronized

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     
     //Variables
     let card_container  = document.getElementById('card_container')
-    let card_number_form  = document.getElementById('card_number')
+    let card_number_form  = document.getElementById('card_number_form')
     let card_number_text = document.getElementById('card_number_text')
 
 
@@ -28,23 +28,60 @@
 
     //Eventos
 
+
+    // ----------------------------------------------- GIRAR TARJETA CON CLICK -----------------------------------------------
+
     let isCardRotated = false
 
     card_container.addEventListener('click', function () {
         isCardRotated = !isCardRotated
 
-        if (isCardRotated) { // true 180 false 0
+        if (isCardRotated) { // aqui en automatico sea verdadero hace 180 y falso 0 (nos ahorra poner mas condiciones)
             rotateContentCard(180)
         } else {
             rotateContentCard(0)
         }
 
+        function rotateContentCard(deg) {
+            card_container.style.transition = '.3s'
+            card_container.style.transform = `rotateY(${deg}deg)` // templates literals (para usar $ para concatenar)
+        }
+
     })
     
-    function rotateContentCard(deg) {
-        card_container.style.transition = '.3s'
-        card_container.style.transform = `rotateY(${deg}deg)` // templates literals (para usar $ para concatenar)
+
+    // ------------------------------------------------------------------------------------------------------------------------
+
+
+
+    // ----------------------------------------------- CAMBIAR NUMEROS AL MISMO TIEMPO ----------------------------------------
+
+
+    card_number_form.addEventListener('input', onChangeCard)
+
+    function onChangeCard() {
+        card_number_text.innerHTML = formatCardNumber(card_number_form.value) //---> card_number_form.value ES EL VALOR QUE SE VA A TOMAR PARA HACER LA FUNCION
+
+        if (card_number_form.value.length> 1) { // si hay algo
+            card_number_form.value = limitLenght(card_number_form.value, 0, 16)
+        }
+
+
+        if (card_number_form.value.length == 0) { // si no hay nada 
+            card_number_text.innerHTML = '#### #### #### ####'
+        }
+
     }
+
+    function formatCardNumber(number){ // -----> ESTA ES LA FUNCION QUE SE VA A USAR PARA REEMPLAZAR
+        return number.replace(/\D/g, '').slice(0, 16).replace(/(.{4})/g, '$1 '); // ---> 0, 16 son los valores que ira cambiando mas de 16 ya no cambiara y el '{4}' es cada cuantos caracteres hay espacio
+    }
+
+    function limitLenght(value, minLength, maxLength) {
+        return value.slice(minLength, maxLength) // ---> va a devolver el valor desde 0 a 16 digitos
+    }
+
+    // ------------------------------------------------------------------------------------------------------------------------
 
 
 })()
